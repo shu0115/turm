f3d561d16f37014fd7a20c78b8904be195db232b
shu0115 <raisondetre0115@gmail.com>
2012-07-06 17:37:47 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Turm::Application.config.secret_token = '10957d7dd86dd45ad2e4c024b072c847e10e0bd351dc4f4b354c7a8b8a965b7e9bff655fab6f58fac1048e77d5e1bd237c7238ac243ea97402dcc4fe22f2eecc'
